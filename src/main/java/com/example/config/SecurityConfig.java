c68ed96a3242ae8d46b3d54cafd0080461bdcd1c
RPC <RPC@DESKTOP-PBRDI46>
2023-11-08 16:19:12 +0900

    20231108_バージョンダウングレード、security導入

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,5 +1,30 @@
 package com.example.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web
+			.ignoring()
+				.antMatchers("/h2-console/**");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+		http
+			.authorizeRequests()
+				.antMatchers("/rpc/**").permitAll()
+				.anyRequest().authenticated();
+
+		http.csrf().disable();
+	}
 
 }
